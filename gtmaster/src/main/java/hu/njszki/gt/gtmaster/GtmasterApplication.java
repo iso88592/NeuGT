b1de48e9fd222f0b7495482c978d0d57304c8e04
Tibor Várkonyi <mocmang@gmail.com>
2019-09-18 18:56:26 +0200

    Add project template

---
+++ gtmaster/src/main/java/hu/njszki/gt/gtmaster/GtmasterApplication.java
@@ -0,0 +1,13 @@
+package hu.njszki.gt.gtmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtmasterApplication.class, args);
+	}
+
+}
